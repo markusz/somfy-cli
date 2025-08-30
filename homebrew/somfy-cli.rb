d9a478f919185f2c38f214a530f46844d4faec0b
Markus Ziller <web+github@markusziller.de>
2025-08-30 12:26:59 +0200

    Update Homebrew formula for v0.2.0

--- homebrew/somfy-cli.rb
+++ homebrew/somfy-cli.rb
@@ -6,20 +6,17 @@ class SomfyCli < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/markusz/somfy-cli/releases/download/v#{version}/somfy-cli-v#{version}.tar.gz"
-      sha256 "Downloading somfy-cli-v0.2.0.tar.gz to calculate SHA256...
-fb2d03e5b166eb64eec453bb7f50bba89621eef3ba1f2f9e6bb875477b71b32d"
+      sha256 "fb2d03e5b166eb64eec453bb7f50bba89621eef3ba1f2f9e6bb875477b71b32d"
     elsif Hardware::CPU.arm?
       url "https://github.com/markusz/somfy-cli/releases/download/v#{version}/somfy-cli-macos-aarch64"
-      sha256 "Downloading somfy-cli-macos-aarch64 to calculate SHA256...
-7de1bc8600a74e2ac72afe059aff145a2545d53fa869a307f35d73ddc6490a60"
+      sha256 "7de1bc8600a74e2ac72afe059aff145a2545d53fa869a307f35d73ddc6490a60"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/markusz/somfy-cli/releases/download/v#{version}/somfy-cli-linux-x86_64"
-      sha256 "Downloading somfy-cli-linux-x86_64 to calculate SHA256...
-1e984d2f59620af63ba8795b86f62341cebe5bea67960146a44a2f2329379188"
+      sha256 "1e984d2f59620af63ba8795b86f62341cebe5bea67960146a44a2f2329379188"
     end
   end
 
